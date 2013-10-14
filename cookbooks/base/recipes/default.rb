6aa5466c1db5edb1c57a850fba67d083c9adfbe6
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-10-14 13:31:25 -0700

    fix error with git defaults that resulted in user names with spaces being truncated`

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -2,13 +2,13 @@ include_recipe "apt"
 include_recipe "build-essential"
 include_recipe "git"
 
-user = node["default"]["user"]
-email = node["default"]["email"]
+user = node["base"]["user"]
+email = node["base"]["email"]
 
 if user
   bash "git user" do
     code <<-EOH
-git config --system user.name #{user}
+git config --system user.name "#{user}"
 EOH
   end
 end
@@ -16,7 +16,7 @@ end
 if email
   bash "git email" do
     code <<-EOH
-git config --system user.email #{email}
+git config --system user.email "#{email}"
 EOH
   end
 end
