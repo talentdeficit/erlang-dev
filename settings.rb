d1fd13430e00848a369845b99694ebea46e34b88
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-10-14 13:32:29 -0700

    anonymize examples in `settings.rb'

--- settings.rb
+++ settings.rb
@@ -4,9 +4,9 @@ VAGRANT_BOX = "precise32"
 
 # though git will use your keys on the host system it won't use your user defaults,
 #  you should set those here
-# GIT_USER = "alisdair sullivan"
+# GIT_USER = "your name"
 GIT_USER = nil
-# GIT_EMAIL = "alisdairsullivan@yahoo.ca"
+# GIT_EMAIL = "your@email.com"
 GIT_EMAIL = nil
 
 
